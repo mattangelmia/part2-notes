a135992cd277ddf7a0f3b5cc451ce089c252a2f5
matt angel <23matthewangel@gmail.com>
2022-01-30 20:10:43 -0500

    added functionality that allows new notes to be added to application

--- src/App.js
+++ src/App.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React , {useState} from 'react'
 import Note from './components/Note'
 
-const App = ({ notes }) => {
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
+  const [newNote, setNewNote] = useState(
+    'a new note...'
+  )
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target)
+    const noteObject = {
+      content: newNote,
+      date: new Date().toISOString(),
+      important: Math.random() < 0.5,
+      id: notes.length + 1,
+    }
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+
+  }
   return (
     <div>
       <h1>Notes</h1>
@@ -10,6 +33,10 @@ const App = ({ notes }) => {
             <Note key={note.id} note={note} />
         )}
       </ul>
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type="submit">save</button>
+      </form> 
     </div>
   )
 }
